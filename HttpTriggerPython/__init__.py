a3f8cd26f969562918c96287e8a65c7d16bfab7b
David Ebbo <david.ebbo@gmail.com>
2018-09-14 16:24:13 -0700

    Test python function

---
+++ HttpTriggerPython/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import platform
+import os
+
+import azure.functions as func
+
+counter = 0
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    global counter
+    counter = counter + 1
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello {name}! FOO={os.environ['FOO']} host={platform.node()} counter={counter}")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400,
+             qqq=123
+        )
